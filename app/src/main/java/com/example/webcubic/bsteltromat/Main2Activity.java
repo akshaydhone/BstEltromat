55b2503cf82012a80aa1dcc9104636f3d499061f
akshay <dhoneakshay39@gmail.com>
2019-06-19 19:01:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webcubic/bsteltromat/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.webcubic.bsteltromat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Main2Activity.this,NewCallGen.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/webcubic/bsteltromat/NewCallGen.java
@@ -0,0 +1,13 @@
+package com.example.webcubic.bsteltromat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCallGen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_call_gen);
+    }
+}
